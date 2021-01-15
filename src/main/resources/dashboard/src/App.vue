11b5a843996f382e56bbadc8ff7d462ec47636d5
liutz <1342677359@qq.com>
2021-01-15 20:08:46 +0800

    static

--- src/main/resources/dashboard/src/App.vue
+++ src/main/resources/dashboard/src/App.vue
@@ -1,72 +1,102 @@
 <template>
-  <div id="app">
-    <el-header>shepherds</el-header>
-    <el-container style="height: 100%">
-      <el-aside width="65px">
-        <el-menu
-          default-active="1-4-1"
-          class="el-menu-vertical-demo"
-          :collapse="isCollapse"
-          style="height:95%"
-        >
-          <el-menu-item index="1">
-            <i class="el-icon-menu"></i>
-            <span slot="title">控制中心</span>
-          </el-menu-item>
-          <el-menu-item index="2">
-            <i class="el-icon-document"></i>
-            <span slot="title">工人管理</span>
-          </el-menu-item>
-          <el-menu-item index="3">
-            <i class="el-icon-plus"></i>
-            <span slot="title">新增工人</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-search"></i>
-            <span slot="title">插件管理</span>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
+    <div id="app">
+        <el-container style="height: 100%;padding-top: 0px">
+            <el-aside width="150px">
+                <el-col :span="15" >
+                    <el-menu default-active="0" >
+                        <p style="font-family: 'Consolas',monospace; font-size: 1.5em;">Shepherds</p>
+                        <el-menu-item index="2" class="menu" v-on:click="load('/views/cont.html')">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title" style="font-size: 20px">控制</span>
+                        </el-menu-item>
+                        <el-menu-item index="3" class="menu" v-on:click="load('/views/manage.html')">
+                            <i class="el-icon-open"></i>
+                            <span slot="title" style="font-size: 20px">管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="4" class="menu" v-on:click="load('/views/add.html')">
+                            <i class="el-icon-plus"></i>
+                            <span slot="title" style="font-size: 20px">添加</span>
+                        </el-menu-item>
+                        <el-menu-item index="5" class="menu" v-on:click="load('/views/reg.html')">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title" style="font-size: 20px">插件</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-col>
+            </el-aside>
 
-      <el-main>Main</el-main>
-    </el-container>
-  </div>
+            <el-main v-html="mainPage">
+            </el-main>
+        </el-container>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "app",
-  data() {
-    return {
-      isCollapse: true,
+    export default {
+        name: "app",
+        data() {
+            return {
+                mainPath: "/views/cont.html",
+                mainPage: "",
+                isCollapse: true,
+            };
+        },
+        methods: {
+            load(url) {
+                this.$http.get(url).then((response) => {
+                    if (response.ok) {
+                        this.mainPage = response.data;
+                    } else {
+                        this.$notify.error({
+                            duration: 1500,
+                            title: response.status,
+                            message: response.statusText
+                        });
+                    }
+                }, (err)=>{
+                    this.$notify.error({
+                        duration: 1500,
+                        title: err.status,
+                        message: err.statusText
+                    });
+                })
+            }
+        },
+        mounted () {
+            this.load("/views/cont.html");
+        },
     };
-  },
-  methods: {},
-};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 10px;
-  height: 100%;
-  width: 100%;
-  position: fixed;
-}
-.el-header,
-.el-footer {
-  color: #333;
-  text-align: left;
-  font-size: 2em;
-  font-family: "Consolas";
-  line-height: 60px;
-  border-bottom-style: solid;
-  border-bottom-width: 3px;
-  border-bottom-color: rgb(38, 97, 97);
-}
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 0;
+        height: 100%;
+        width: 100%;
+        position: fixed;
+    }
+    .el-aside {
+        padding: 0;
+        margin: 0;
+        box-shadow: 2px 0 10px rgba(0,0,0,.2);
+    }
+    .el-col.el-col-15 {
+        width: 100%;
+    }
+    .menu {
+        margin-left: -30px;
+    }
+    .el-menu-item.is-active {
+        background-color: rgba(0,128,128,0.1);
+    }
 
 </style>
--- src/main/resources/dashboard/src/main.js
+++ src/main/resources/dashboard/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.http.options.emulateJSON = true;
 
 new Vue({
   render: h => h(App),
